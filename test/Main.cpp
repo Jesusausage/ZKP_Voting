847cce71b301b52da54aee4ce2b73c6ba82ae5fa
Philip Cai <philipcai@hotmail.co.uk>
2019-07-08 19:58:55 +0100

    Delete outmoded test files

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,8 +1,3 @@
-#include "GroupTest.hpp"
-#include "ModularIntTest.hpp"
-#include "SigmaUtilsTest.hpp"
-#include "SchnorrTest.hpp"
-#include "ElgamalTest.hpp"
 #include "SchnorrProtocolTest.hpp"
 #include "OrProtocolTest.hpp"
 #include "ECGroupTest.hpp"
@@ -12,30 +7,6 @@
 
 
 int main() {
-    TestModularIntConstructor();
-    TestMultiply();
-    TestAssignment();
-    std::cout << "ModularInt test success." << std::endl;
-    
-    TestGroupConstructor();
-    TestPower();
-    TestInverse();
-    std::cout << "Group test success." << std::endl;
-
-    TestGetPrime();
-    TestGetGroup();
-    std::cout << "SigmaUtils test success." << std::endl;
-    
-    TestGenerateCommitment();
-    TestVerification();
-    TestSimulation();
-    std::cout << "Schnorr test success." << std::endl;
-
-    TestGenerateElgamalCommitment();
-    TestElgamalVerification();
-    TestElgamalSimulation();
-    std::cout << "Elgamal test success." << std::endl;
-
     TestNormalSchnorrRun();
     TestSimulatedSchnorrRun();
     std::cout << "SchnorrProtocol test success." << std::endl;
