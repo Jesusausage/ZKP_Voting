8538cfba5389842d7a4b94d78d19d749c6b1ac71
Philip Cai <philipcai@hotmail.co.uk>
2019-07-11 14:53:29 +0100

    create votertest src file

---
+++ test/VoterTest.cpp
@@ -0,0 +1,27 @@
+#include "VoterTest.hpp"
+
+
+void TestVoting()
+{
+    auto ecg = GenerateECGroup();
+
+    auto gen = ecg.base;
+
+    auto id_key = RandomInteger(2, ecg.order);
+    auto id = ecg.curve.Multiply(id_key, gen);
+    auto id_sum = ecg.curve.Multiply(27, gen);
+    IDInfo id_info = {id, id_sum, id_key};
+
+    std::vector<VoteTokenInfo> token_info;
+    for (int i = 0; i < 4; i++) {
+        auto token_key = RandomInteger(2, ecg.order);
+        auto token = ecg.curve.Multiply(token_key, gen);
+        auto token_sum = ecg.curve.Multiply(42+i, gen);
+        token_info.push_back({token, token_sum, token_key});
+    }
+
+    Voter voter(ecg, gen, id_info, token_info);
+
+    voter.castVote(3);
+    Vote vote = voter.getVoteAndProofs();
+}
