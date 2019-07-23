6d775afe1438211bb5687c5bf6a61cfd7c6efb75
Philip Cai <philipcai@hotmail.co.uk>
2019-07-23 23:47:37 +0100

    constructor/destructor for bootstrapper

--- include/Bootstrapper.hpp
+++ include/Bootstrapper.hpp
@@ -7,17 +7,18 @@
 
 class Bootstrapper {
 public:
+    Bootstrapper(int num_voters, int num_options);
+    ~Bootstrapper();
 
 private:
     CryptoPP::ECPPoint* voter_ids_;
-    int num_voters;
+    int num_voters_;
 
     CryptoPP::ECPPoint** tokens_;
     int num_options_;
     std::string* options_;
 
     std::string* ip_addrs_;
-
 };
 
 
--- src/Bootstrapper.cpp
+++ src/Bootstrapper.cpp
@@ -1,2 +1,29 @@
 #include "Bootstrapper.hpp"
 
+
+Bootstrapper::Bootstrapper(int num_voters, int num_options)
+                           :
+                           num_voters_(num_voters), num_options_(num_options)
+{
+    voter_ids_ = new CryptoPP::ECPPoint[num_voters_];
+
+    tokens_ = new CryptoPP::ECPPoint*[num_voters_];
+    for (int i = 0; i < num_voters_; i++)
+        tokens_[i] = new CryptoPP::ECPPoint[num_options_];
+
+    options_ = new std::string[num_options_];
+
+    ip_addrs_ = new std::string[num_voters_];
+}
+
+
+Bootstrapper::~Bootstrapper()
+{
+    delete [] voter_ids_;
+    for (int i = 0; i < num_voters_; i++)
+        delete [] tokens_[i];
+    delete [] tokens_;
+
+    delete [] options_;
+    delete [] ip_addrs_;
+}
