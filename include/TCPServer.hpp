86c318d26f0c523e4a0e229e03eb188a02eabd65
Philip Cai <philipcai@hotmail.co.uk>
2019-07-28 22:07:20 +0100

    Create TCP Server

---
+++ include/TCPServer.hpp
@@ -0,0 +1,18 @@
+#ifndef VOTE_DATA_HPP
+#define VOTE_DATA_HPP
+
+
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+
+
+class TCPServer {
+public:
+
+private:
+};
+
+
+#endif
