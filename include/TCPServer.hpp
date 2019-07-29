6f86e893929c55e519d8811f713992ebc73cf677
Philip Cai <philipcai@hotmail.co.uk>
2019-07-30 00:36:57 +0100

    basic TCPServer functions

--- include/TCPServer.hpp
+++ include/TCPServer.hpp
@@ -8,10 +8,33 @@
 #include <boost/asio.hpp>
 
 
-class TCPServer {
+class TCPConnection : public boost::enable_shared_from_this<TCPConnection> {
 public:
+    static boost::shared_ptr<TCPConnection> create(boost::asio::io_context& io_context);
+    void start();
+    boost::asio::ip::tcp::socket& socket();
 
 private:
+    boost::asio::ip::tcp::socket socket_;
+    std::string some_stuff_;
+
+    TCPConnection(boost::asio::io_context& io_context);
+
+    void handleWrite(const boost::system::error_code&, size_t);
+};
+
+
+class TCPServer {
+public:
+    TCPServer(boost::asio::io_context& io_context);
+
+private:
+    boost::asio::io_context& io_context_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+
+    void startAccept();
+    void handleAccept(boost::shared_ptr<TCPConnection> new_connection,
+                      const boost::system::error_code& error);
 };
 
 
--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -1,1 +1,69 @@
 #include "TCPServer.hpp"
+
+
+using namespace boost::asio::ip;
+
+
+TCPServer::TCPServer(boost::asio::io_context& io_context)
+                     :
+                     io_context_(io_context),
+                     acceptor_(io_context, tcp::endpoint(tcp::v4(), 1337))
+{
+    startAccept();
+}
+
+
+void TCPServer::startAccept()
+{
+    boost::shared_ptr<TCPConnection> new_connection = TCPConnection::create(io_context_);
+    acceptor_.async_accept(new_connection->socket(), 
+                           boost::bind(&TCPServer::handleAccept, this,
+                                       new_connection,
+                                       boost::asio::placeholders::error));
+}
+
+
+void TCPServer::handleAccept(boost::shared_ptr<TCPConnection> new_connection,
+                             const boost::system::error_code& error)
+{
+    if (!error)
+        new_connection->start();
+    
+    startAccept();
+}
+
+
+/* ======================================================================== */
+
+
+boost::shared_ptr<TCPConnection> TCPConnection::create(boost::asio::io_context& io_context)
+{
+    return boost::shared_ptr<TCPConnection>(new TCPConnection(io_context));
+}
+
+
+void TCPConnection::start()
+{
+    // async_write some_stuff_ through socket_
+    boost::asio::async_write(socket_, boost::asio::buffer(some_stuff_),
+                             boost::bind(&TCPConnection::handleWrite, shared_from_this(),
+                                         boost::asio::placeholders::error,
+                                         boost::asio::placeholders::bytes_transferred));
+}
+
+
+tcp::socket& TCPConnection::socket()
+{
+    return socket_;
+}
+
+
+TCPConnection::TCPConnection(boost::asio::io_context& io_context)
+                             :
+                             socket_(io_context)
+{}
+
+
+void TCPConnection::handleWrite(const boost::system::error_code&, size_t)
+{
+}
