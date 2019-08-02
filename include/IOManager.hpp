abc339ab843891678ed34ad61719ea346d7ee717
Philip Cai <philipcai@hotmail.co.uk>
2019-08-02 16:01:24 +0100

    IOManager class

---
+++ include/IOManager.hpp
@@ -0,0 +1,17 @@
+#ifndef IO_MANAGER_HPP
+#define IO_MANAGER_HPP
+
+
+#include "VoteData.hpp"
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+
+class IOManager {
+public:
+
+private:
+};
+
+
+#endif
