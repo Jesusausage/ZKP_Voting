fb59491c7106d09680fb3553b4d58f5809064bc5
Philip Cai <philipcai@hotmail.co.uk>
2019-08-09 00:01:36 +0100

    shorten private_data class

--- include/PrivateData.hpp
+++ include/PrivateData.hpp
@@ -2,39 +2,27 @@
 #define PRIV_DATA_HPP
 
 
-#include "PublicData.hpp"
 #include "Errors.hpp"
+#include <cryptopp/integer.h>
 #include <vector>
 #include <fstream>
 
 
-#define VOTE_FILE "output/votes"
-#define KEY_FILE "output/keys"
 #define PRIV_KEY_FILE "private_keys.txt"
 
 
-class PrivateData
-{
+class PrivateData {
 public:
-    PrivateData(const ECGroup &ecg, const CryptoPP::ECPPoint &generator,
-                const PublicData &pub_data);
+    PrivateData(int num_options);
 
-    inline int voterIndex() const 
-        { return voter_index_; }
     inline const CryptoPP::Integer& idKey() const
         { return id_key_; }
     inline const std::vector<CryptoPP::Integer>& tokenKeys() const
         { return token_keys_; }
 
 private:
-    const PublicData &dat_;
-
-    int voter_index_;
     CryptoPP::Integer id_key_;
     std::vector<CryptoPP::Integer> token_keys_;
-
-    void readPrivFromFile();
-    void findVoterIndex(const ECGroup& ecg, const CryptoPP::ECPPoint& gen);
 };
 
 
--- src/PrivateData.cpp
+++ src/PrivateData.cpp
@@ -1,27 +1,16 @@
 #include "PrivateData.hpp"
 
 
-PrivateData::PrivateData(const ECGroup& ecg, 
-                         const CryptoPP::ECPPoint& generator,
-                         const PublicData& pub_data)
-                         :
-                         dat_(pub_data)
-{
-    readPrivFromFile();
-    findVoterIndex(ecg, generator);
-}
-
-
-void PrivateData::readPrivFromFile()
+PrivateData::PrivateData(int num_options)
 {
     std::fstream priv_in(PRIV_KEY_FILE, std::ios::in);
     if (!priv_in.is_open()) {
         std::cerr << "Error opening " << PRIV_KEY_FILE << std::endl;
         exit(FILE_OPENING_ERROR);
     }
-    token_keys_.resize(dat_.numOptions());
+    token_keys_.resize(num_options);
     priv_in >> id_key_ >> std::ws;
-    for (int i = 0; i < dat_.numOptions(); ++i) {
+    for (int i = 0; i < num_options; ++i) {
         if (priv_in.eof()) {
             std::cerr << "Incomplete private keys file: " << PRIV_KEY_FILE << std::endl;
             exit(INCOMPLETE_FILE_ERROR);
@@ -29,17 +18,4 @@ void PrivateData::readPrivFromFile()
         priv_in >> token_keys_[i] >> std::ws;
     }
     priv_in.close();
-}
-
-
-void PrivateData::findVoterIndex(const ECGroup& ecg, 
-                                 const CryptoPP::ECPPoint& gen)
-{
-    auto id = ecg.curve.Multiply(id_key_, gen);
-    for (int i = 0; i < dat_.numVoters(); ++i) {
-        if (dat_.id(i) == id) {
-            voter_index_ = i;
-            return;
-        }
-    }
 }
